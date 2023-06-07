9178b6472b1c380fffc64bd32f774609666709a1
Vinicius Carneiro Gonçalves <105397334+viniciuscg@users.noreply.github.com>
2023-06-06 23:31:30 -0300

    ajustes api

--- src/movie-finder-frontend/src/services/api.js
+++ src/movie-finder-frontend/src/services/api.js
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: "https://projetopucmoviefinder.azurewebsites.net/v1/public"
-    // baseURL: "https://localhost:44307/v1/public"
 })
